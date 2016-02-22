a3cc2d5137f9dce633131a0722922c721084ea22
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-02-22 01:57:21 +0000

    Fix the enum value used for the sample count

--- src/fv-main.c
+++ src/fv-main.c
@@ -1142,7 +1142,7 @@ init_vk(struct data *data)
                 },
                 .mipLevels = 1,
                 .arrayLayers = 1,
-                .samples = 1,
+                .samples = VK_SAMPLE_COUNT_1_BIT,
                 .tiling = VK_IMAGE_TILING_OPTIMAL,
                 .usage = (VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
                           VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT),
