6ddbe66c6b9717c1a16a9840a3bd77cd6d3fe99b
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-10-02 09:14:07 +0200

    Revert "error-message: Just use fprintf instead of SDL"
    Now that SDL is back we can use it for the error messages again.
    
    This reverts commit 27d9d1eb2280a2f8cb95544c0b3d38e5516204fb.

--- src/fv-error-message.c
+++ src/fv-error-message.c
@@ -20,9 +20,15 @@
 #include "config.h"
 
 #include <stdio.h>
+#include <SDL.h>
 #include <stdarg.h>
 
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
 #include "fv-error-message.h"
+#include "fv-buffer.h"
 
 void
 fv_error_message(const char *format, ...)
@@ -30,7 +36,23 @@ fv_error_message(const char *format, ...)
         va_list ap;
 
         va_start(ap, format);
-        vfprintf(stderr, format, ap);
-        fputc('\n', stderr);
+#ifndef WIN32
+        if (isatty(STDERR_FILENO)) {
+                vfprintf(stderr, format, ap);
+                fputc('\n', stderr);
+        } else
+#endif
+        {
+                struct fv_buffer buffer;
+
+                fv_buffer_init(&buffer);
+                fv_buffer_append_vprintf(&buffer, format, ap);
+                SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                                         "Finvenkisto - eraro",
+                                         (char *) buffer.data,
+                                         NULL);
+                fv_buffer_destroy(&buffer);
+        }
+
         va_end(ap);
 }
