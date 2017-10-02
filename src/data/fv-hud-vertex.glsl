3dcd82e8c2807d49578e2c9112d4633041de04f6
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-10-02 17:40:00 +0200

    hud: Flip the y-coordinate in the vertices
    This makes the code the same as the Vulkan branch so it’s a bit easier
    to maintain.

--- src/data/fv-hud-vertex.glsl
+++ src/data/fv-hud-vertex.glsl
@@ -25,7 +25,7 @@ varying vec2 tex_coord;
 void
 main()
 {
-        gl_Position = vec4(position, 0.0, 1.0);
+        gl_Position = vec4(position.x, -position.y, 0.0, 1.0);
         tex_coord = tex_coord_attrib;
 }
 
--- src/fv-hud.c
+++ src/fv-hud.c
@@ -1,7 +1,7 @@
 /*
  * Finvenkisto
  *
- * Copyright (C) 2014, 2015 Neil Roberts
+ * Copyright (C) 2014, 2015, 2017 Neil Roberts
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -150,10 +150,10 @@ fv_hud_new(struct fv_image_data *image_data,
 
         for (i = 0; i < FV_HUD_MAX_RECTANGLES; i++) {
                 elements[i * 6 + 0] = i * 4 + 0;
-                elements[i * 6 + 1] = i * 4 + 1;
-                elements[i * 6 + 2] = i * 4 + 3;
-                elements[i * 6 + 3] = i * 4 + 3;
-                elements[i * 6 + 4] = i * 4 + 1;
+                elements[i * 6 + 1] = i * 4 + 3;
+                elements[i * 6 + 2] = i * 4 + 1;
+                elements[i * 6 + 3] = i * 4 + 1;
+                elements[i * 6 + 4] = i * 4 + 3;
                 elements[i * 6 + 5] = i * 4 + 2;
         }
 
@@ -220,9 +220,9 @@ fv_hud_add_rectangle(struct fv_hud *hud,
         x2 = (x + image->w) * 2.0f / hud->screen_width - 1.0f;
         y2 = (y + image->h) * 2.0f / hud->screen_height - 1.0f;
         s1 = image->x / (float) hud->tex_width;
-        t1 = (image->y + image->h) / (float) hud->tex_height;
+        t1 = image->y / (float) hud->tex_height;
         s2 = (image->x + image->w) / (float) hud->tex_width;
-        t2 = image->y / (float) hud->tex_height;
+        t2 = (image->y + image->h) / (float) hud->tex_height;
 
         hud->vertex->x = x1;
         hud->vertex->y = y1;
@@ -290,7 +290,7 @@ fv_hud_add_title(struct fv_hud *hud)
 {
         fv_hud_add_rectangle(hud,
                              hud->screen_width / 2 - fv_hud_image_title.w / 2,
-                             hud->screen_height / 2,
+                             hud->screen_height / 2 - fv_hud_image_title.h,
                              &fv_hud_image_title);
 }
 
@@ -304,9 +304,7 @@ fv_hud_paint_player_select(struct fv_hud *hud,
         fv_hud_add_rectangle(hud,
                              screen_width / 2 -
                              fv_hud_image_player_select.w / 2,
-                             screen_height / 2 -
-                             fv_hud_image_player_select.h -
-                             10,
+                             screen_height / 2 + 10,
                              &fv_hud_image_player_select);
         fv_hud_end_rectangles(hud);
 }
@@ -332,7 +330,7 @@ fv_hud_paint_key_select(struct fv_hud *hud,
                 x = (screen_width / 2 -
                      fv_hud_image_push.w / 2 -
                      key_image->w / 2);
-                y = screen_height / 2 - fv_hud_image_push.h - 10;
+                y = screen_height / 2 + 10;
                 fv_hud_add_rectangle(hud, x, y, &fv_hud_image_push);
                 fv_hud_add_rectangle(hud,
                                      x + fv_hud_image_push.w, y,
@@ -342,12 +340,15 @@ fv_hud_paint_key_select(struct fv_hud *hud,
                      fv_hud_image_push.w / 2 +
                      player_num % 2 * screen_width / 2);
                 y = (screen_height / 4 +
-                     (1 - player_num / 2) * screen_height / 2);
-                fv_hud_add_rectangle(hud, x, y, &fv_hud_image_push);
+                     (player_num / 2) * screen_height / 2);
+                fv_hud_add_rectangle(hud,
+                                     x,
+                                     y - fv_hud_image_push.h,
+                                     &fv_hud_image_push);
                 fv_hud_add_rectangle(hud,
                                      x +
                                      (fv_hud_image_push.w - key_image->w) / 2,
-                                     y - key_image->h,
+                                     y,
                                      key_image);
         }
 
@@ -440,7 +441,7 @@ fv_hud_add_scores(struct fv_hud *hud,
         fv_hud_add_number(hud,
                           &fv_hud_image_star,
                           fv_logic_get_score(logic, 0),
-                          0, screen_height - fv_hud_digit_images[0]->h,
+                          0, 0, /* x/y */
                           FV_HUD_ALIGNMENT_LEFT);
 
         if (n_players < 2)
@@ -450,7 +451,7 @@ fv_hud_add_scores(struct fv_hud *hud,
                           &fv_hud_image_star,
                           fv_logic_get_score(logic, 1),
                           screen_width,
-                          screen_height - fv_hud_digit_images[0]->h,
+                          0, /* y */
                           FV_HUD_ALIGNMENT_RIGHT);
 
         if (n_players < 3)
@@ -459,7 +460,7 @@ fv_hud_add_scores(struct fv_hud *hud,
         fv_hud_add_number(hud,
                           &fv_hud_image_star,
                           fv_logic_get_score(logic, 2),
-                          0, 0,
+                          0, screen_height - fv_hud_digit_images[0]->h,
                           FV_HUD_ALIGNMENT_LEFT);
 
         if (n_players < 4)
@@ -468,7 +469,8 @@ fv_hud_add_scores(struct fv_hud *hud,
         fv_hud_add_number(hud,
                           &fv_hud_image_star,
                           fv_logic_get_score(logic, 3),
-                          screen_width, 0,
+                          screen_width,
+                          screen_height - fv_hud_digit_images[0]->h,
                           FV_HUD_ALIGNMENT_RIGHT);
 }
 
@@ -486,7 +488,7 @@ fv_hud_add_fina_venko(struct fv_hud *hud,
                                fv_hud_image_fina.w,
                                FV_HUD_FINA_VENKO_SLIDE_TIME);
         fv_hud_add_rectangle(hud,
-                             x, screen_height / 2,
+                             x, screen_height / 2 - fv_hud_image_fina.h,
                              &fv_hud_image_fina);
 
         if (fina_venko_time >= FV_HUD_FINA_VENKO_SLIDE_TIME / 2.0f) {
@@ -500,7 +502,7 @@ fv_hud_add_fina_venko(struct fv_hud *hud,
                                        FV_HUD_FINA_VENKO_SLIDE_TIME);
                 fv_hud_add_rectangle(hud,
                                      x,
-                                     screen_height / 2 - fv_hud_image_venko.h,
+                                     screen_height / 2,
                                      &fv_hud_image_venko);
         }
 }
@@ -523,11 +525,11 @@ fv_hud_paint_game_state(struct fv_hud *hud,
 
         if (fv_logic_get_n_players(logic) == 1) {
                 crocodile_x = screen_width;
-                crocodile_y = screen_height - fv_hud_digit_images[0]->h;
+                crocodile_y = 0;
                 crocodile_alignment = FV_HUD_ALIGNMENT_RIGHT;
         } else {
                 crocodile_x = screen_width / 2;
-                crocodile_y = screen_height - fv_hud_digit_images[0]->h;
+                crocodile_y = 0;
                 crocodile_alignment = FV_HUD_ALIGNMENT_CENTER;
         }
 
