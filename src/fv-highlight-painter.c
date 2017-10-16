5ff5dadde698ca0e5478d95c110b0d005eeaf31e
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-10-16 21:32:51 +0200

    highlight-painter: Don’t have a hardcoded number of highlights
    Instead it now allocates a buffer and recreates it on the fly if it is
    too small.

--- src/fv-highlight-painter.c
+++ src/fv-highlight-painter.c
@@ -34,14 +34,7 @@
 #include "fv-flush-memory.h"
 #include "fv-map.h"
 
-#define FV_HIGHLIGHT_PAINTER_MAX_HIGHLIGHTS (FV_MAP_TILES_X + 1 +       \
-                                             FV_MAP_TILES_Y + 1 +       \
-                                             (FV_MAP_WIDTH *          \
-                                              FV_MAP_HEIGHT) +         \
-                                             1)
-#define FV_HIGHLIGHT_PAINTER_INDICES_OFFSET             \
-        (sizeof (struct fv_vertex_highlight) * 4 *      \
-         FV_HIGHLIGHT_PAINTER_MAX_HIGHLIGHTS)
+#define HIGHLIGHT_SIZE (sizeof (struct fv_vertex_highlight) * 4)
 
 struct fv_highlight_painter {
         const struct fv_vk_data *vk_data;
@@ -51,25 +44,23 @@ struct fv_highlight_painter {
         VkBuffer vertex_buffer;
         VkDeviceMemory vertex_memory;
         int vertex_memory_type_index;
-        VkDeviceSize vertex_memory_watermark;
+        VkDeviceSize vertex_buffer_size;
 
         struct fv_vertex_highlight *vertex_memory_map;
-
-        /* Count of highlights that have been placed in the buffer since
-         * the last frame was started */
-        int buffer_offset;
 };
 
 static bool
-create_buffer(struct fv_highlight_painter *painter)
+create_vertex_buffer(struct fv_highlight_painter *painter)
 {
+        uint16_t *indices;
         VkResult res;
+        int i;
 
         VkBufferCreateInfo buffer_create_info = {
                 .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
-                .size = (FV_HIGHLIGHT_PAINTER_INDICES_OFFSET +
+                .size = (HIGHLIGHT_SIZE * painter->vertex_buffer_size +
                          sizeof (uint16_t) *
-                         FV_HIGHLIGHT_PAINTER_MAX_HIGHLIGHTS * 6),
+                         painter->vertex_buffer_size * 6),
                 .usage = (VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
                           VK_BUFFER_USAGE_INDEX_BUFFER_BIT),
                 .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
@@ -109,30 +100,10 @@ create_buffer(struct fv_highlight_painter *painter)
                 return false;
         }
 
-        return true;
-}
-
-struct fv_highlight_painter *
-fv_highlight_painter_new(const struct fv_vk_data *vk_data,
-                         const struct fv_pipeline_data *pipeline_data)
-{
-        struct fv_highlight_painter *painter = fv_calloc(sizeof *painter);
-        uint16_t *indices;
-        int i;
-
-        painter->vk_data = vk_data;
-        painter->pipeline =
-                pipeline_data->pipelines[FV_PIPELINE_DATA_PIPELINE_HIGHLIGHT];
-        painter->layout =
-                pipeline_data->layouts[FV_PIPELINE_DATA_LAYOUT_SHOUT];
-
-        if (!create_buffer(painter))
-                goto error;
-
         indices = (uint16_t *) ((uint8_t *) painter->vertex_memory_map +
-                                FV_HIGHLIGHT_PAINTER_INDICES_OFFSET);
+                                HIGHLIGHT_SIZE * painter->vertex_buffer_size);
 
-        for (i = 0; i < FV_HIGHLIGHT_PAINTER_MAX_HIGHLIGHTS; i++) {
+        for (i = 0; i < painter->vertex_buffer_size; i++) {
                 indices[i * 6 + 0] = i * 4 + 0;
                 indices[i * 6 + 1] = i * 4 + 1;
                 indices[i * 6 + 2] = i * 4 + 2;
@@ -146,11 +117,22 @@ fv_highlight_painter_new(const struct fv_vk_data *vk_data,
                         painter->vertex_memory,
                         VK_WHOLE_SIZE);
 
-        return painter;
+        return true;
+}
 
-error:
-        fv_highlight_painter_free(painter);
-        return NULL;
+struct fv_highlight_painter *
+fv_highlight_painter_new(const struct fv_vk_data *vk_data,
+                         const struct fv_pipeline_data *pipeline_data)
+{
+        struct fv_highlight_painter *painter = fv_calloc(sizeof *painter);
+
+        painter->vk_data = vk_data;
+        painter->pipeline =
+                pipeline_data->pipelines[FV_PIPELINE_DATA_PIPELINE_HIGHLIGHT];
+        painter->layout =
+                pipeline_data->layouts[FV_PIPELINE_DATA_LAYOUT_SHOUT];
+
+        return painter;
 }
 
 static void
@@ -184,6 +166,30 @@ set_highlight(struct fv_vertex_highlight *vertex,
         vertex++;
 }
 
+static void
+free_vertex_buffer(struct fv_highlight_painter *painter)
+{
+        if (painter->vertex_memory_map) {
+                fv_vk.vkUnmapMemory(painter->vk_data->device,
+                                    painter->vertex_memory);
+                painter->vertex_memory_map = NULL;
+        }
+
+        if (painter->vertex_memory) {
+                fv_vk.vkFreeMemory(painter->vk_data->device,
+                                   painter->vertex_memory,
+                                   NULL /* allocator */);
+                painter->vertex_memory = NULL;
+        }
+        if (painter->vertex_buffer) {
+                fv_vk.vkDestroyBuffer(painter->vk_data->device,
+                                      painter->vertex_buffer,
+                                      NULL /* allocator */);
+                painter->vertex_buffer = NULL;
+        }
+        painter->vertex_buffer_size = 0;
+}
+
 void
 fv_highlight_painter_paint(struct fv_highlight_painter *painter,
                            VkCommandBuffer command_buffer,
@@ -196,10 +202,23 @@ fv_highlight_painter_paint(struct fv_highlight_painter *painter,
         VkDeviceSize vertices_offset = 0;
         size_t i;
 
-        assert(painter->buffer_offset + n_highlights <=
-               FV_HIGHLIGHT_PAINTER_MAX_HIGHLIGHTS);
+        if (n_highlights <= 0)
+                return;
 
-        vertex = painter->vertex_memory_map + painter->buffer_offset * 4;
+        if (n_highlights > painter->vertex_buffer_size) {
+                free_vertex_buffer(painter);
+
+                painter->vertex_buffer_size = 1;
+                while (painter->vertex_buffer_size < n_highlights)
+                        painter->vertex_buffer_size *= 2;
+
+                if (!create_vertex_buffer(painter)) {
+                        free_vertex_buffer(painter);
+                        return;
+                }
+        }
+
+        vertex = painter->vertex_memory_map;
 
         for (i = 0; i < n_highlights; i++) {
                 set_highlight(vertex, highlights + i);
@@ -225,7 +244,8 @@ fv_highlight_painter_paint(struct fv_highlight_painter *painter,
                                      &vertices_offset);
         fv_vk.vkCmdBindIndexBuffer(command_buffer,
                                    painter->vertex_buffer,
-                                   FV_HIGHLIGHT_PAINTER_INDICES_OFFSET,
+                                   painter->vertex_buffer_size *
+                                   HIGHLIGHT_SIZE,
                                    VK_INDEX_TYPE_UINT16);
         fv_vk.vkCmdDrawIndexed(command_buffer,
                                n_highlights * 6,
@@ -234,46 +254,25 @@ fv_highlight_painter_paint(struct fv_highlight_painter *painter,
                                0, /* vertexOffset */
                                0 /* firstInstance */);
 
-        painter->buffer_offset += n_highlights;
-        painter->vertex_memory_watermark =
-                painter->buffer_offset *
-                sizeof (struct fv_vertex_highlight) * 4;
+        fv_flush_memory(painter->vk_data,
+                        painter->vertex_memory_type_index,
+                        painter->vertex_memory,
+                        n_highlights * HIGHLIGHT_SIZE);
 }
 
 void
 fv_highlight_painter_begin_frame(struct fv_highlight_painter *painter)
 {
-        painter->buffer_offset = 0;
-        painter->vertex_memory_watermark = 0;
 }
 
 void
 fv_highlight_painter_end_frame(struct fv_highlight_painter *painter)
 {
-        fv_flush_memory(painter->vk_data,
-                        painter->vertex_memory_type_index,
-                        painter->vertex_memory,
-                        painter->vertex_memory_watermark);
 }
 
 void
 fv_highlight_painter_free(struct fv_highlight_painter *painter)
 {
-        if (painter->vertex_memory_map) {
-                fv_vk.vkUnmapMemory(painter->vk_data->device,
-                                    painter->vertex_memory);
-        }
-
-        if (painter->vertex_memory) {
-                fv_vk.vkFreeMemory(painter->vk_data->device,
-                                   painter->vertex_memory,
-                                   NULL /* allocator */);
-        }
-        if (painter->vertex_buffer) {
-                fv_vk.vkDestroyBuffer(painter->vk_data->device,
-                                      painter->vertex_buffer,
-                                      NULL /* allocator */);
-        }
-
+        free_vertex_buffer(painter);
         fv_free(painter);
 }
